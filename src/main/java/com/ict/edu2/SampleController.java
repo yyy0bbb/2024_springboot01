7b7dbdfe1acb9f9aa08dd21c8de5991dd2c0feb1
JEONYOUNGBIN <wjsdudqls0931@naver.com>
2024-11-15 15:50:43 +0900

    세 번째 변경사항

--- src/main/java/com/ict/edu2/SampleController.java
+++ src/main/java/com/ict/edu2/SampleController.java
@@ -4,10 +4,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -62,7 +64,7 @@ public class SampleController {
     }
 
     @GetMapping("/hello5")
-    public DataVO getHello5(String number) {
+    public DataVO getHello5(@RequestParam String number) {
         DataVO dataVO = new DataVO();
 
         List<SampleVO> list = new ArrayList<>();
@@ -101,10 +103,98 @@ public class SampleController {
         return "hi, 안녕, ㅎㅇㅎㅇㅎㅇ" ;
     }
     
+    
     @PostMapping("/hi2")
+    //일반적인 파라미터는 get/post가 같다
     public String[] getHi2(String idx) {
         return new String[] {"Hello", "World"};
     }
     
+    @PostMapping("/hi3")
+    //일반적인 파라미터는 get/post가 같다
+    public String[] getHi3(@RequestParam("idx") String idx) {
+        return new String[] {"Hello", "World"};
+    }
+
+    @PostMapping("/hi4")
+    //요청자체가 json 방식일 때
+    public String[] getHi4(@RequestBody Map<String, String> params) {
+        return new String[] {"Hello", "World"};
+    }
+    
+
+
+    @PostMapping("/hi5")
+    public SampleVO getHi5() {
+        SampleVO svo = new SampleVO();
+        svo.setName("고길동");
+        svo.setAge("24");
+
+        return svo;
+    }
+
+    @PostMapping("/hi6")
+    public DataVO getHi6(@RequestParam String number) {
+        DataVO dataVO = new DataVO();
+
+        List<SampleVO> list = new ArrayList<>();
+
+        SampleVO svo = new SampleVO("고길동","14");
+        SampleVO svo2 = new SampleVO("강길동","61");
+        SampleVO svo3 = new SampleVO("김길동","443");
+        list.add(svo);
+        list.add(svo2);
+        list.add(svo3);
+
+        if(number.equals("1")){
+            // dataVO.setResultCode(1);
+            // dataVO.setMessage("성공");
+            // dataVO.setData(list);
+
+            return DataVO.success(list, "성공");
+
+        }else{
+            // dataVO.setResultCode(0);
+            // dataVO.setMessage("실패");
+            // dataVO.setData(null);
+
+            return DataVO.fail(0, "실패");
+        }
+
+       
+    }
+
+
+    @PostMapping("/hi7")
+    public DataVO getHi7(@RequestParam String number) {
+        DataVO dataVO = new DataVO();
+
+        List<SampleVO> list = new ArrayList<>();
+
+        SampleVO svo = new SampleVO("고길동","84");
+        SampleVO svo2 = new SampleVO("강길동","31");
+        SampleVO svo3 = new SampleVO("김길동","13");
+        list.add(svo);
+        list.add(svo2);
+        list.add(svo3);
+
+        if(number.equals("1")){
+            // dataVO.setResultCode(1);
+            // dataVO.setMessage("성공");
+            // dataVO.setData(list);
+
+            return DataVO.success(list, "성공");
+
+        }else{
+            // dataVO.setResultCode(0);
+            // dataVO.setMessage("실패");
+            // dataVO.setData(null);
+
+            return DataVO.fail(0, "실패");
+        }
+
+       
+    }
+
     
 }
